3cdb614cdad243e48b09bae7ce98c95edf0601eb
Yundi Huang <kenkaka.hyd@gmail.com>
2014-10-24 18:09:01 -0400

    major updates, still have bugs

---
+++ KHSideNav/KHSideNav.swift
@@ -0,0 +1,9 @@
+//
+//  KHSideNav.swift
+//  KHSideNav
+//
+//  Created by Ken Huang on 2014-10-13.
+//  Copyright (c) 2014 Ken Huang. All rights reserved.
+//
+
+import Foundation
---
+++ KHSideNav/KHSideViewController.swift
@@ -0,0 +1,9 @@
+//
+//  KHSideViewController.swift
+//  KHSideNav
+//
+//  Created by Ken Huang on 2014-10-22.
+//  Copyright (c) 2014 Ken Huang. All rights reserved.
+//
+
+import Foundation
